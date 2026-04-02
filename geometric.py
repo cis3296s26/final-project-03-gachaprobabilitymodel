7cfba09717a5b8597b8e8b1da68022ab602f5131
ElamTemple <tur71527@temple.edu>
2026-04-02 13:14:48 -0400

    Created geometric py

--- geometric.py
+++ geometric.py
@@ -1,3 +1,17 @@
 import math
 import random
+from collections import Counter
 
+def gachaModel(currency: int, cost: int, rate: float, seed: int): 
+    if currency < cost:
+        print ("Not enough currency to pull.")
+    random.seed(seed)
+
+    rolls = currency // cost
+    pulls = []
+    successes = 0
+    firstHit = None
+
+    for i in range (1 , rolls + 1):
+        if random.random() < rate:
+            int = 0
