3b4e4fbcdd84ba789e0211aa73b1392fe4462b7f
ElamTemple <tur71527@temple.edu>
2026-04-03 11:35:10 -0400

    finished geometric function math

--- geometric.py
+++ geometric.py
@@ -14,4 +14,17 @@ def gachaModel(currency: int, cost: int, rate: float, seed: int):
 
     for i in range (1 , rolls + 1):
         if random.random() < rate:
-            int = 0
+            successes += 1
+            pulls.append(i)
+            if firstHit is None:
+                firstHit = i
+
+    empMean = sum(rolls) / successes if successes > 0 else 0
+    theoMean = 1 / rate
+    theoVariance = (1 - rate) / (rate ** 2)
+
+    empSuccessRate = successes / rolls
+
+    pmf = {k: ((1 - rate) ** (k - 1)) * rate for k in range(1, rolls + 1)}
+    cdf = {k: 1 - (1 - rate) ** k for k in range(1, rolls + 1)}
+    
