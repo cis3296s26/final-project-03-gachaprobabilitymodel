7914d0f6dc4199aabebccacc47f1b799c95151f2
ElamTemple <tur71527@temple.edu>
2026-04-23 12:35:40 -0400

    Bug fix on geometric import gachamodel line

--- Histogram.py
+++ Histogram.py
@@ -3,7 +3,7 @@ import random
 from collections import Counter
 from typing import Optional, List, Dict, Any, Tuple
 
-from geometric import gachaModel, rateCalculator, GachaHistogram as GeometricHistogram
+from geometric import gachaModel
 from FGO import FGOrate, check_fgo_featured, FGORateCalc
 from Umamusume import UMArate, check_uma_featured, UMARateCalc
 from HoyoverseGames import HoyoverseRate, HoyoversePitySystem, hoyoverseRateCalc
